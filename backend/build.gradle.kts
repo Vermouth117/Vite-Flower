3161743fab3eff5b7fe18c8c7fc1c49b09662e80
Vermouth117 <e153jsb7eg@gmail.com>
2024-01-24 02:41:19 +0900

    update: appspec.yml and buildspec.yml

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -23,6 +23,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.flywaydb:flyway-core:9.22.3")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
