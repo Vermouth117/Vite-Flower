4778470d1cf03139b71f9334f2f09bfc60301594
Vermouth117 <e153jsb7eg@gmail.com>
2024-01-23 23:29:33 +0900

    update: appspec.yml and buildspec.yml

--- deploy_script.sh
+++ deploy_script.sh
@@ -2,7 +2,7 @@
 
 cd /var/www/html/
 sudo
-chmod u+x /var/html/flower-0.0.1-SNAPSHOT.jar
+#chmod u+x /var/html/flower-0.0.1-SNAPSHOT.jar
 #/usr/bin/java -jar ./flower-0.0.1-SNAPSHOT.jar
 
 java -jar ./flower-0.0.1-SNAPSHOT.jar \
