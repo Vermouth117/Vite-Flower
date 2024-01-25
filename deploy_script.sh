a1e7f99c9a418435085f808bcebaef5e28e0d652
Vermouth117 <e153jsb7eg@gmail.com>
2024-01-26 02:30:49 +0900

    buildspec-for-ec2.yml

--- deploy_script.sh
+++ deploy_script.sh
@@ -3,5 +3,4 @@
 cd /var/www/html/
 sudo chown ec2-user:ec2-user ./flower-0.0.1-SNAPSHOT.log
 sudo chmod +w ./flower-0.0.1-SNAPSHOT.log
-java -jar ./flower-0.0.1-SNAPSHOT.jar &
-#java -jar ./flower-0.0.1-SNAPSHOT.jar >> ./flower-0.0.1-SNAPSHOT.log 2>&1 &
+java -jar ./flower-0.0.1-SNAPSHOT.jar >> ./flower-0.0.1-SNAPSHOT.log 2>&1 &
